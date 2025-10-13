0691e7170483e8e1d9f752f4c08c2479b7ad7912
nickdutto <5254128+nickdutto@users.noreply.github.com>
2025-10-13 15:30:40 +1100

    feat(prettier): add tool - prettier

---
+++ tools/prettier/index.js
@@ -0,0 +1,36 @@
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig } */
+const config = {
+  trailingComma: "all",
+  tabWidth: 2,
+  printWidth: 100,
+  semi: true,
+  endOfLine: "auto",
+  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  importOrder: [
+    "<TYPES>",
+    "",
+    "<BUILTIN_MODULES>",
+    "",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^@react-gis-openlayers",
+    "",
+    "^@react-gis-openlayers/(.*)$",
+    "",
+    "<TYPES>^[.|..|~]",
+    "^~/",
+    "",
+    "^[../]",
+    "",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.9.0",
+  tailwindAttributes: ["className", "classNames"],
+  tailwindFunctions: ["clsx", "cn", "tv"],
+};
+
+export default config;
